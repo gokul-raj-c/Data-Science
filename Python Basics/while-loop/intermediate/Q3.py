5e7fdb02425354e7330a6de1edd3ae4921654dbd
gokulrajc8393 <gokulrajc63@gmail.com>
2026-07-13 20:41:24 +0530

    while loop

--- Python Basics/while-loop/intermediate/Q3.py
+++ Python Basics/while-loop/intermediate/Q3.py
@@ -1,9 +1,8 @@
 #Take an integer from the user and print its digits in reverse using a while loop (e.g. 1234 -> 4 3 2 1)
 
 n=int(input("enter number: "))
-rev=0
 while n>0:
     r=n%10
-    rev=rev*10+r
+    #rev=rev*10+r
+    print(r)
     n=n//10
-print(rev)
